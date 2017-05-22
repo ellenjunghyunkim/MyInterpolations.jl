7de9841bb66a5c138c41843ba52bb317dab988a5
ellenjunghyunkim <ellenmilano@naver.com>
2017-05-22 15:22:44 +0900

    edited

--- src/MyInterpolations.jl
+++ src/MyInterpolations.jl
@@ -7,25 +7,14 @@ module MyLinInterp
     vals::Array
 　end
 
-　function Base.call(a::LinearInterpolation, x::Real)
-    i = searchsortedlast(a.grid, x)
-    if i == 0 || i == length(a.grid)
+　function (f::LinearInterpolation)(x)
+    i = searchsortedlast(f.grid, x)
+    if i == 0 || i == length(f.grid)
         return 0
  　　end
-    interpolated_value = (a.vals[i+1]-a.vals[i])/(a.grid[i+1]-a.grid[i])*(x-a.grid[i])+a.vals[i]
+    interpolated_value = (f.vals[i+1]-f.vals[i])/(f.grid[i+1]-f.grid[i])*(x-f.grid[i])+f.vals[i]
     return interpolated_value
 　　　
-　end
-
-　function Base.call{T<:Real}(a::LinearInterpolation, x::AbstractVector{T})
-    n = length(x)
-    out = Array(Float64, n)
-
-    for i in 1:n
-        out[i] = a(x[i])
-    end
-
-    return out
 　end
 　　
 
