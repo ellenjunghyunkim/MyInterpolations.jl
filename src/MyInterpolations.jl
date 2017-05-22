87f22eda0cdbfe3a0739fbcea76daab2fe13acb3
ellenjunghyunkim <ellenmilano@naver.com>
2017-05-22 15:59:02 +0900

    no message

--- src/MyInterpolations.jl
+++ src/MyInterpolations.jl
@@ -9,7 +9,7 @@ module MyInterpolations
 
 　function (f::LinearInterpolation)(x)
     i = searchsortedlast(f.grid, x)
-    if i == 0 || ( i == length(f.grid) && f.grid[end]! = x )
+    if i == 0 || ( i == length(f.grid) && f.grid[end] !=  x )
         return 0
  　　end
     interpolated_value = (f.vals[i+1]-f.vals[i])/(f.grid[i+1]-f.grid[i])*(x-f.grid[i])+f.vals[i]
